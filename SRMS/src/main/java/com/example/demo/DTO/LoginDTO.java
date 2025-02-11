8cca636e76e289206453a3166b15b821a3c541e8
Teja Kumar M <tejakumarm13@gmail.com>
2025-02-11 19:40:28 +0530

    Initial commit: add front‑end and back‑end projects 2

--- SRMS/src/main/java/com/example/demo/DTO/LoginDTO.java
+++ SRMS/src/main/java/com/example/demo/DTO/LoginDTO.java
@@ -1,26 +1,28 @@
 package com.example.demo.DTO;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
+import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginDTO {
-    public String getUsername() {
+	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	private String username;
-    private String password;
+	private String password;
 }
--- SRMS/src/main/java/com/example/demo/DTO/QueryDTO.java
+++ SRMS/src/main/java/com/example/demo/DTO/QueryDTO.java
@@ -1,6 +1,5 @@
 package com.example.demo.DTO;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,24 +7,29 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
+
 public class QueryDTO {
 	public QueryDTO(String username, String query) {
-        this.username = username;
-        this.query = query;
-    }
+		this.username = username;
+		this.query = query;
+	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getQuery() {
 		return query;
 	}
+
 	public void setQuery(String query) {
 		this.query = query;
 	}
+
 	private String username;
-    private String query;
+	private String query;
 }
